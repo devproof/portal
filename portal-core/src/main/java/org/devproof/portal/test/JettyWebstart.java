b60daa90710d0ee620b84f82dea2db8822972037
Carsten Hufe <carsten.hufe@devproof.org>
2009-06-21 15:43:15 +0000

    Java Webstart

--- portal-core/src/main/java/org/devproof/portal/test/JettyWebstart.java
+++ portal-core/src/main/java/org/devproof/portal/test/JettyWebstart.java
@@ -15,15 +15,17 @@
  */
 package org.devproof.portal.test;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Session;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.naming.NamingException;
 
+import org.apache.commons.lang.UnhandledException;
 import org.apache.wicket.protocol.http.WicketFilter;
 import org.devproof.portal.core.app.PortalApplication;
 import org.devproof.portal.core.module.common.CommonConstants;
@@ -43,17 +45,19 @@ import org.springframework.web.context.ContextLoaderListener;
  * @author Carsten Hufe
  */
 public class JettyWebstart {
+	private SocketConnector connector;
+	private Server server;
 
-	public static void main(final String[] args) throws Exception {
-		Server server = new Server();
-		SocketConnector connector = new SocketConnector();
+	public JettyWebstart() {
+		this.server = new Server();
+		this.connector = new SocketConnector();
 		// Set some timeout options to make debugging easier.
-		connector.setMaxIdleTime(1000 * 60 * 60);
-		connector.setSoLingerTime(-1);
-		connector.setPort(8888);
-		server.setConnectors(new Connector[] { connector });
+		this.connector.setMaxIdleTime(1000 * 60 * 60);
+		this.connector.setSoLingerTime(-1);
+
+		this.server.setConnectors(new Connector[] { this.connector });
 		WebAppContext bb = new WebAppContext();
-		bb.setServer(server);
+		bb.setServer(this.server);
 		bb.setContextPath("/");
 		bb.setWar(System.getProperty("java.io.tmpdir"));
 		bb.addEventListener(new ContextLoaderListener());
@@ -66,38 +70,45 @@ public class JettyWebstart {
 		servlet.setClassName(WicketFilter.class.getName());
 		servlet.setName(WicketFilter.class.getName());
 		bb.addFilter(servlet, "/*", 0);
-		server.addHandler(bb);
+		this.server.addHandler(bb);
 
 		SimpleDriverDataSource datasource = new SimpleDriverDataSource();
 		datasource.setUrl("jdbc:hsqldb:mem:testdb");
 		datasource.setUsername("sa");
 		datasource.setPassword("");
 		datasource.setDriverClass(org.hsqldb.jdbcDriver.class);
-		new Resource(CommonConstants.JNDI_DATASOURCE, datasource);
-		Properties props = System.getProperties();
-		Session mailSession = Session.getDefaultInstance(props);
-		new Resource(CommonConstants.JNDI_MAIL_SESSION, mailSession);
-		new Resource(CommonConstants.JNDI_PROP_HIBERNATE_DIALECT, HSQLDialect.class.getName());
-		PortalTestUtil.createDataStructure(Arrays.asList("file:///E:/Workspaces/devproof/portal-webapp/target/sql/install_devproof_hsql.sql"));
-		// PortalTestUtil.createDataStructure(Arrays.asList("install_devproof_hsql.sql"));
 		try {
-			System.out.println(">>> STARTING DEVPROOF PORTAL, PRESS ANY KEY TO STOP");
-			server.start();
-			// while (true) {
-			// Thread.sleep(5000);
-			// }
-			// server.stop();
-			// server.join();
-			JFrame frame = new JFrame("Hallo1");
-			frame.show();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			new Resource(CommonConstants.JNDI_DATASOURCE, datasource);
+			Properties props = System.getProperties();
+			Session mailSession = Session.getDefaultInstance(props);
+			new Resource(CommonConstants.JNDI_MAIL_SESSION, mailSession);
+			new Resource(CommonConstants.JNDI_PROP_HIBERNATE_DIALECT, HSQLDialect.class.getName());
+			PortalTestUtil.createDataStructure(Arrays.asList("file:///E:/Workspaces/devproof/portal-webapp/target/sql/install_devproof_hsql.sql"));
+			// PortalTestUtil.createDataStructure(Arrays.asList("install_devproof_hsql.sql"));
+		} catch (NamingException e) {
+			throw new UnhandledException(e);
+		} catch (SQLException e) {
+			throw new UnhandledException(e);
+		} catch (IOException e) {
+			throw new UnhandledException(e);
+		}
+	}
+
+	public void startServer(final int port) {
+		this.connector.setPort(port);
+		try {
+			this.server.start();
 		} catch (Exception e) {
-			e.printStackTrace();
-			JFrame frame = new JFrame("Hallo1");
-			frame.add(new JTextArea(e.toString()));
-			frame.show();
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			// System.exit(100);
+			throw new UnhandledException(e);
+		}
+	}
+
+	public void stopServer() {
+		try {
+			this.server.stop();
+			this.server.join();
+		} catch (Exception e) {
+			throw new UnhandledException(e);
 		}
 	}
 }
