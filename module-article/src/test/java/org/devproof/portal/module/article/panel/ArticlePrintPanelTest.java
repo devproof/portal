b5a7e6e1b131e2d5f0bf452575fefbc0e03584df
Carsten Hufe <carsten.hufe@devproof.org>
2009-12-20 02:37:19 +0000

    more tests

---
+++ module-article/src/test/java/org/devproof/portal/module/article/panel/ArticlePrintPanelTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2009-2010 Carsten Hufe devproof.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.devproof.portal.module.article.panel;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.util.tester.TestPanelSource;
+import org.apache.wicket.util.tester.WicketTester;
+import org.devproof.portal.module.article.entity.ArticleEntity;
+import org.devproof.portal.test.PortalTestUtil;
+
+/**
+ * @author Carsten Hufe
+ */
+public class ArticlePrintPanelTest extends TestCase {
+	private WicketTester tester;
+
+	@Override
+	public void setUp() throws Exception {
+		tester = PortalTestUtil.createWicketTesterWithSpringAndDatabase("create_tables_hsql_article.sql",
+				"insert_article.sql");
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		PortalTestUtil.destroy(tester);
+	}
+
+	public void testRenderDefaultPage() {
+		tester.startPanel(createArticlePrintPanel());
+		tester.assertComponent("panel", ArticlePrintPanel.class);
+	}
+
+	private TestPanelSource createArticlePrintPanel() {
+		return new TestPanelSource() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public Panel getTestPanel(String panelId) {
+				ArticleEntity article = new ArticleEntity();
+				article.setTitle("foo");
+				article.setTeaser("bar");
+				article.setCreatedAt(new Date());
+				article.setModifiedAt(new Date());
+				article.setCreatedBy("foo");
+				article.setModifiedBy("foo");
+				return new ArticlePrintPanel(panelId, article);
+			}
+		};
+	}
+}
