3d5af593668b9b85a7f165fcb6841f78b56076a8
Carsten Hufe <carsten.hufe@devproof.org>
2009-12-25 21:26:09 +0000

    initial test

---
+++ portal-core/src/test/java/org/devproof/portal/core/module/common/dao/DataProviderDaoImplTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.devproof.portal.core.module.common.dao;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Carsten Hufe
+ */
+public class DataProviderDaoImplTest extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindAllClassOfT() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindAllClassOfTIntInt() {
+		fail("Not yet implemented");
+	}
+
+	public void testFindAllWithQuery() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSizeClassOfTSerializable() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSizeClassOfTStringSerializable() {
+		fail("Not yet implemented");
+	}
+}
