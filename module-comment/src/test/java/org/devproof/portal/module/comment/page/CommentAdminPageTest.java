631ebc13bc64170ffb1085f7d702132dd1846e9d
Carsten Hufe <carsten.hufe@devproof.org>
2010-03-13 20:10:09 +0000

    last comment test

---
+++ module-comment/src/test/java/org/devproof/portal/module/comment/page/CommentAdminPageTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2009-2010 Carsten Hufe devproof.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *   
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.devproof.portal.module.comment.page;
+
+import junit.framework.TestCase;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.devproof.portal.test.PortalTestUtil;
+
+/**
+ * @author Carsten Hufe
+ */
+public class CommentAdminPageTest extends TestCase {
+	private WicketTester tester;
+
+	@Override
+	public void setUp() throws Exception {
+		tester = PortalTestUtil.createWicketTesterWithSpringAndDatabase("create_tables_hsql_comment.sql",
+				"insert_comment.sql");
+		PortalTestUtil.loginDefaultAdminUser(tester);
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		PortalTestUtil.destroy(tester);
+	}
+
+	public void testRenderDefaultPage() {
+		tester.startPage(CommentAdminPage.class);
+		tester.assertRenderedPage(CommentAdminPage.class);
+	}
+}
