f2ed20874a7f65d7b824fbbe3523aedc0d968120
Carsten Hufe <carsten.hufe@comsysto.com>
2010-10-27 13:48:31 +0200

    annotations added

---
+++ portal-core/src/main/java/org/devproof/portal/core/annotation/DevproofPage.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2009-2010 Carsten Hufe devproof.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devproof.portal.core.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Marking a devproof page for mounting etc.
+ *
+ * @author Carsten Hufe
+ */
+@Documented
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface DevproofPage {
+    /**
+     * Mount path
+     */
+    String mountPath();
+
+    /**
+     * Register main navigation link for this page
+     */
+    boolean registerMainNavigationLink() default false;
+
+    /**
+     * Register global administration link
+     */
+    boolean registerGlobalAdminLink() default false;
+
+    /**
+     * Mount the page indexed without parameter names
+     */
+    boolean indexMountedPath() default false;
+
+    /**
+     * Should this page the default start page when you call /
+     */
+    boolean defaultStartPage() default false;
+
+    /**
+     * Register this page in the administration links
+     */
+    boolean registerPageAdminLink() default false;
+}
