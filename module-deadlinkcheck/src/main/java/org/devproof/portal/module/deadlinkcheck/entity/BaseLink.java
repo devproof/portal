4f990a95c556dcfcf749c8ad3af113fc903c7c3e
Carsten Hufe <carsten.hufe@comsysto.com>
2010-11-04 12:10:16 +0100

    BaseLinkEntity to BaseLink renamed

---
+++ module-deadlinkcheck/src/main/java/org/devproof/portal/module/deadlinkcheck/entity/BaseLink.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2009-2010 Carsten Hufe devproof.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devproof.portal.module.deadlinkcheck.entity;
+
+import org.devproof.portal.core.module.common.entity.BaseEntity;
+
+import javax.persistence.*;
+
+/**
+ * Base entity for bookmarks and downloads
+ *
+ * @author Carsten Hufe
+ */
+@MappedSuperclass
+public abstract class BaseLink extends BaseEntity {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "title")
+    private String title;
+    @Lob
+    @Column(name = "description")
+    private String description;
+    @Column(name = "url")
+    private String url;
+    @Column(name = "hits")
+    private Integer hits = 0;
+    @Column(name = "number_of_votes")
+    private Integer numberOfVotes = 0;
+    @Column(name = "sum_of_rating")
+    private Integer sumOfRating = 0;
+    @Column(name = "broken")
+    private Boolean broken = Boolean.FALSE;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getHits() {
+        return hits;
+    }
+
+    public void setHits(Integer hits) {
+        this.hits = hits;
+    }
+
+    public Integer getNumberOfVotes() {
+        return numberOfVotes;
+    }
+
+    public void setNumberOfVotes(Integer numberOfVotes) {
+        this.numberOfVotes = numberOfVotes;
+    }
+
+    public Integer getSumOfRating() {
+        return sumOfRating;
+    }
+
+    public void setSumOfRating(Integer sumOfRating) {
+        this.sumOfRating = sumOfRating;
+    }
+
+    public Boolean getBroken() {
+        return broken;
+    }
+
+    public void setBroken(Boolean broken) {
+        this.broken = broken;
+    }
+
+    @Transient
+    public Double getCalculatedRating() {
+        double back = ((double) sumOfRating) / ((double) numberOfVotes);
+        if (Double.isInfinite(back) || Double.isNaN(back)) {
+            back = 0d;
+        }
+        return back;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        BaseLink other = (BaseLink) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
