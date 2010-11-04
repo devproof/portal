23af82119fbabbd9c805d0b5e5c75b4ff658e86e
Carsten Hufe <carsten.hufe@comsysto.com>
2010-11-04 18:21:36 +0100

    new sql scripts

--- portal-core/src/main/resources/sql/update/update_from_1.0_to_1.1-rc1_mysql_core.sql
+++ portal-core/src/main/resources/sql/update/update_from_1.0_to_1.1-rc1_mysql_core.sql
@@ -1,13 +1,6 @@
-UPDATE core_configuration
-
-
-ERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.forgotemail','Forgot your password email','Email','java.lang.Integer','2');
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.reconfirmemail','Reconfirmation email, when email was changed','Email','java.lang.Integer','5');
-
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.contactformemail','Contact form email template','Email','java.lang.Integer','6');
-
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.regemail','Registration email','Email','java.lang.Integer','1');
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.registereduser','Notification: A new user has been registered','Email','java.lang.Integer','3');
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.emailTemplateRepository.findAll.subject.id.unknownerror','Notification: Unknown error email','Email','java.lang.Integer','4');
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.roleRepository.findAll.description.id.guestrole','Default guest role','User','java.lang.Integer','2');
-INSERT INTO core_configuration (conf_key,conf_description,conf_group,conf_type,conf_value) VALUES ('spring.roleRepository.findAll.description.id.registerrole','Default role for registration','User','java.lang.Integer','3');
+UPDATE core_configuration set conf_key = 'spring.emailTemplateRepository.findAll.subject.id.forgotemail'
+  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.forgotemail';
+UPDATE core_configuration set conf_key = 'spring.emailTemplateRepository.findAll.subject.id.reconfirmemail'
+  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.reconfirmemail';
+UPDATE core_configuration set conf_key = 'spring.emailTemplateRepository.findAll.subject.id.contactformemail'
+  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.contactformemail';
