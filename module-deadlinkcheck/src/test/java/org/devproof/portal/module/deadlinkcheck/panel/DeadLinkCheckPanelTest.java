fe202bf2e8f77d6beca56531bc76537b3e3d7680
Carsten Hufe <carsten.hufe@devproof.org>
2010-11-06 03:42:47 +0100

    Pleasetuff for deadlink check

--- module-deadlinkcheck/src/test/java/org/devproof/portal/module/deadlinkcheck/panel/DeadLinkCheckPanelTest.java
+++ module-deadlinkcheck/src/test/java/org/devproof/portal/module/deadlinkcheck/panel/DeadLinkCheckPanelTest.java
@@ -19,22 +19,33 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.util.tester.WicketTester;
 import org.devproof.portal.module.deadlinkcheck.entity.BaseLink;
+import org.devproof.portal.test.MockContextLoader;
 import org.devproof.portal.test.PortalTestUtil;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.servlet.ServletContext;
 import java.util.ArrayList;
 
 /**
  * @author Carsten Hufe
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = MockContextLoader.class,
+        locations = {"classpath:/org/devproof/portal/module/deadlinkcheck/test-datasource.xml" })
 public class DeadLinkCheckPanelTest {
+    @Autowired
+    private ServletContext servletContext;
     private WicketTester tester;
 
     @Before
     public void setUp() throws Exception {
-        tester = PortalTestUtil.createWicketTesterWithSpringAndDatabase();
+        tester = PortalTestUtil.createWicketTester(servletContext);
     }
 
     @After
