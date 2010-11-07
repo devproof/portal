5ac41416af4d5155cf0dfa7d6d459036137a97b2
Carsten Hufe <carsten.hufe@devproof.org>
2010-11-08 00:05:32 +0100

    renamed test

--- portal-core/src/test/java/org/devproof/portal/core/module/common/repository/DataProviderRepositoryImplTest.java
+++ portal-core/src/test/java/org/devproof/portal/core/module/common/repository/DataProviderRepositoryImplTest.java
@@ -39,7 +39,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * @author Carsten Hufe
  */
-public class DataProviderDaoImplTest {
+public class DataProviderRepositoryImplTest {
 	private DataProviderRepositoryImpl<EmailTemplate> impl;
 	private SessionFactory sessionFactory;
 	private Session session;
