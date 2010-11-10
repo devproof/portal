0aa2b1ac04efa7307052a33fdf6039e3454fa92e
Carsten Hufe <carsten.hufe@devproof.org>
2010-11-10 21:54:19 +0100

    sql fixes

--- module-comment/src/main/resources/sql/update/update_from_1.0_to_1.1-rc1_oracle_core.sql
+++
@@ -1,6 +0,0 @@
-UPDATE core_configuration set conf_key = 'spring.emailService.findAll.subject.id.newcommentnotification'
-  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.newcommentnotification';
-UPDATE core_configuration set conf_key = 'spring.emailService.findAll.subject.id.autoblockednotification'
-  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.autoblockednotification';
-UPDATE core_configuration set conf_key = 'spring.emailService.findAll.subject.id.violationnotification'
-  WHERE conf_key = 'spring.emailTemplateDao.findAll.subject.id.violationnotification';
