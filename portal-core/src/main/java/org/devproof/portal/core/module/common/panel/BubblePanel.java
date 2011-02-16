d8ee2051313f30d835aaa93ae980dff8783b548f
Carsten Hufe <carsten.hufe@comsysto.com>
2011-02-16 18:31:26 +0100

    damn ie fix ...

--- portal-core/src/main/java/org/devproof/portal/core/module/common/panel/BubblePanel.java
+++ portal-core/src/main/java/org/devproof/portal/core/module/common/panel/BubblePanel.java
@@ -93,9 +93,10 @@ public class BubblePanel extends Panel {
     public void showModal(AjaxRequestTarget target) {
         target.addComponent(this);
         String js = "";
-        js = "var maskHeight = $(document).height(); var maskWidth = $(window).width();";
-        js += "$('#modalMask').css({'width':maskWidth,'height':maskHeight});";
-        js += "$('#modalMask').fadeTo('normal',0.3);";
+//        js = "var maskHeight = $(document).height(); var maskWidth = $(window).width();";
+        js += "$('#modalMask').css({'width':$(window).width(),'height':$(document).height()});";
+//        js += "$('#modalMask').fadeTo('normal',0.3);";
+        js += "$('#modalMask').fadeIn('normal');";
         // js +=
         // "$('#modalMask').fadeIn('fast'); $('#modalMask').fadeTo('fast',0.3);";
         js += "var id = '#" + getMarkupId() + "';";
